2732324b35fbbe83e2939d69192a9d0a40d4a78b
Boluwatife Omosowon <boluomosowon@gmail.com>
2020-07-15 21:53:20 +0100

    Faculty and UI added

---
+++ Data/StaffPortalDataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using StaffPortal.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace StaffPortal.Data
+{
+    public class StaffPortalDataContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public StaffPortalDataContext(DbContextOptions<StaffPortalDataContext> options) : base(options)
+        {
+        }
+        public virtual DbSet<Faculty> Authors { get; set; }
+       
+    }
+}
