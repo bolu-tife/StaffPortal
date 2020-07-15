ea7281b1d9fc97056c67401ac2c1b530897a04df
Stephanie <efuayegodwin@gmail.com>
2020-07-15 22:34:07 +0100

    Dept and Grade added

---
+++ Entities/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffPortal.Entities
+{
+    public class Department
+    {
+        public int DeptCode { get; set; }
+        public string DeptName { get; set; }
+        //public Faculty Faculty { get; set; }
+    }
+}
---
+++ Entities/Grade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffPortal.Entities
+{
+    public class Grade
+    {
+        public string GradeName { get; set; }
+        public int Level { get; set; }
+        public int Step { get; set; }
+
+    }
+}
